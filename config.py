c9df81ae1992cebcd0580b6ca80f434b239ccd79
Chaitu-07 <153836785+Chaitu-07@users.noreply.github.com>
2026-08-13 23:47:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
 
     SECRET_KEY = os.getenv("SECRET_KEY")
 
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.getenv("postgresql://crime_pattern_db_user:INZYhXBHQ5fqXqsnMQUsqRAmfxR2AxAP@dpg-d9v0f9ajobas73bvnl5g-a/crime_pattern_db")
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
